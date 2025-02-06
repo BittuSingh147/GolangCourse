68371871edcec820bc1faeb23d45948adf08c2e9
BittuSingh147 <19bcs1715@gmail.com>
2025-02-06 19:24:17 +0530

    Run Hello World

---
+++ HelloWorld/main.go
@@ -0,0 +1,9 @@
+package main //package is a keyword that is used to declare a package name
+
+import "fmt"
+//fmt is a package that contains functions for printing formatted output and scanning input
+//func is a keyword that is used to declare a function
+func main()  {
+	fmt.Println("Hello World!")
+	
+}
