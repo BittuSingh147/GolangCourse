2597e68154f11bd489ba765757c84d2952b64372
BittuSingh147 <19bcs1715@gmail.com>
2025-02-06 19:35:51 +0530

    Simple value is golang

---
+++ 02_SimpleValues/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	//int in go
+	fmt.Println(1 + 1)
+
+	//String in go
+	fmt.Println("This is a string")
+
+	//Bool in go
+	fmt.Println(true)
+
+	//Float in go
+
+	fmt.Println(10.5)
+
+	//we can also add float in arithmatic operations
+	fmt.Println(6.5 + 6.7)
+}
