8aecd7e6147ac1e3858fff0374b3158b632b8985
BittuSingh147 <19bcs1715@gmail.com>
2025-02-06 19:51:16 +0530

    Variables in Go

---
+++ 03_Variables/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main(){
+	// var name string = "Bittu"
+	
+
+	//we can also do like this
+	// var name="Bittu"
+	name := "Bittu"//this will also work same as above
+	fmt.Println(name)
+
+//for declaring integer
+	// var age int = 21;
+	// var age = 21;
+	age:=21
+	fmt.Println(age)
+
+
+
+	//for bool
+	//var iSEmployed bool = true
+	var iSEmployed = true
+	fmt.Println(iSEmployed)
+
+	//for float
+
+	var decimal float32=10.5
+	fmt.Println(decimal)
+	// fmt.Println(decimal*2) we can also set arithmetic operation as well
+
+}
