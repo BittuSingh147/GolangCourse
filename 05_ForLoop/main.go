b26eaa25be488c7b0b3e80eeb2b8836e464618be
BittuSingh147 <19bcs1715@gmail.com>
2025-02-06 20:20:31 +0530

    For in go

---
+++ 05_ForLoop/main.go
@@ -0,0 +1,34 @@
+package main
+
+func main() {
+	//while loop
+
+	// i:=1
+	// for i<=3{
+	// 	println(i)
+	// 	i=i+1
+	// }
+
+	//also we have infinite loop
+	// i:=3
+	// for {
+	// 	println(i)
+	// }
+
+	//we also have classic for loop
+
+	// for i:=0; i<10; i++{
+	// 	// break
+	// 	if i==2{
+	// 		continue
+	// 	}
+	// 	println(i)
+	// }
+
+	//we can also use for loop with range
+
+	for i := range 3 {
+		println(i)
+	}
+
+}
